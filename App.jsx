7e4c9ff27abc086b68f6747f6ccf2939e6594e37
Nickolas <nickolaslira.564@gmail.com>
2024-02-21 09:39:16 -0300

    estilizaçao

--- App.jsx
+++ App.jsx
@@ -1,12 +1,15 @@
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import react from "react";
+import React from "react";
+import { useState } from "react";
 
 export default function App() {
+const [task, setTaks] = useState("");
 	return (
-		<View style={styles.container}>
+		<View style={styles.container} >
   <View>
     <TextInput
+      value={task}
       placeholder="Digite uma nova tarefa"
       style={styles.input}
     />
@@ -14,7 +17,7 @@ export default function App() {
       style={styles.button}
       onPress={() => setTaks("")}
     >
-      <Text style={`text-white`}>Adicionar</Text>
+      <Text style={styles.buttonText}>Adicionar</Text>
     </TouchableOpacity>
   </View>
   <StatusBar style="auto" />
@@ -23,11 +26,37 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      back
-    }
-
-
-
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form:{
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: "#000",
+    width: "100%",
+    padding: 5,
+  },
+  button:{
+    backgroundColor: "#008000",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText:{
+    color: "#fff",
+  },
+  list:{
+    marginTop: 20,
+  },
+  item:{
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    marginVertical: 5,
+  },
+});
