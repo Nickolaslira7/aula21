bae455aae3d5a92105ea58477570d4105ec48dfe
Nickolas <nickolaslira.564@gmail.com>
2024-02-21 09:36:56 -0300

    commit

--- App.jsx
+++ App.jsx
@@ -1,11 +1,33 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import react from "react";
 
 export default function App() {
 	return (
-		<View>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
+		<View style={styles.container}>
+  <View>
+    <TextInput
+      placeholder="Digite uma nova tarefa"
+      style={styles.input}
+    />
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => setTaks("")}
+    >
+      <Text style={`text-white`}>Adicionar</Text>
+    </TouchableOpacity>
+  </View>
+  <StatusBar style="auto" />
+</View>
 	);
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      back
+    }
+
+
+
+})
